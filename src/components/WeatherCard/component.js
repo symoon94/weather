a0abddfde815f5b9e0636ed1066def3b7c972ff7
symoon94 <msy0128@gmail.com>
2020-05-13 15:47:03 -0700

    md red value

--- src/components/WeatherCard/component.js
+++ src/components/WeatherCard/component.js
@@ -6,7 +6,7 @@ import Condition from "./Condition";
 import Icon from "./Icon";
 
 const WeatherCard = (props) => {
-  const red = 100;
+  const red = 200;
   const Card = styled.div`
     margin: 0 auto;
     background: linear-gradient(to bottom, rgba(${red}, 200, 200), lightblue);
