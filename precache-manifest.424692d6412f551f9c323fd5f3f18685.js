963ac1c98b54c68755c6f7314b75f0c94f627ce2
symoon94 <msy0128@gmail.com>
2020-05-13 15:41:47 -0700

    Updates

---
+++ precache-manifest.424692d6412f551f9c323fd5f3f18685.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ff007c3e54e7e96ffe016a3d9e22805",
+    "url": "/symoon94.github.io/weather/index.html"
+  },
+  {
+    "revision": "5f06c8b569d4d09c35fa",
+    "url": "/symoon94.github.io/weather/static/css/main.0fe2ecf8.chunk.css"
+  },
+  {
+    "revision": "a470a79098bb76a01380",
+    "url": "/symoon94.github.io/weather/static/js/2.3653a6eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/symoon94.github.io/weather/static/js/2.3653a6eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f06c8b569d4d09c35fa",
+    "url": "/symoon94.github.io/weather/static/js/main.73896f35.chunk.js"
+  },
+  {
+    "revision": "6bc57d9c21fab297a1db",
+    "url": "/symoon94.github.io/weather/static/js/runtime-main.5ea431eb.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/weather/precache-manifest.839942febcc333ddf17715fc25f47a8b.js"
+  "/symoon94.github.io/weather/precache-manifest.424692d6412f551f9c323fd5f3f18685.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/weather/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/symoon94.github.io/weather/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
